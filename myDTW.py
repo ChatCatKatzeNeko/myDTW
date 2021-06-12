2e5cc10ac172acaec173c5dfa11c7b2a38aca312
Siyun <39126293+ChatCatKatzeNeko@users.noreply.github.com>
2021-06-12 20:29:13 +0200

    Update myDTW.py

--- myDTW.py
+++ myDTW.py
@@ -30,7 +30,7 @@ class myDTW():
             self.distMat = np.ones((len(shorter)+1,len(longer)+1))*np.inf
             self.distMat[0,0] = 0
             for i in range(1,len(shorter)+1):
-                for j in range(max(1, i-window+1),min(len(longer)+1,i+diff+window)): #%diff
+                for j in range(max(1, i-window+1),min(len(longer)+1,i+diff+window)): 
                     val1 = abs(shorter[i-1]-longer[j-1])
                     val2 = min(self.distMat[i-1, j-1], self.distMat[i-1,j], self.distMat[i,j-1])
                     self.distMat[i,j] = val1 + val2
@@ -46,7 +46,6 @@ class myDTW():
         self.axis1 = [j]
         while (i>1) | (j>1):
             i, j = self._helper_get_next_pivot(i, j)
-            #self.distance += self.distMat[i,j]
             self.axis0.append(i)
             self.axis1.append(j)
             self.pathLength += 1
@@ -62,8 +61,11 @@ class myDTW():
         return i-(1-minIndices[0]), j-(1-minIndices[1])
     
     def plot_optimal_path(self):
+        '''
+        plot the optimal back tracking path
+        '''
         matcp = self.distMat.copy()
-        matcp[self.axis0, self.axis1] = 1e6
+        matcp[self.axis0, self.axis1] = 1e6 # initialising the distance matrix to a large number (1e6 in this case) in order to make the path stand out from the background when plotted
         fig = plt.figure(figsize=(12,12))
         G = plt.GridSpec(ncols=4,nrows=4,figure=fig)
         
@@ -78,10 +80,21 @@ class myDTW():
         axPath.set(xticks=[], yticks=[])
         plt.show()
     
+    # "main" function of the class: compute the DTW distance
     def compute(self, a, b, window=None, returnLength=False):
+        '''
+        return the DTW distance between two input time series
+        
+        INPUTS:
+        a,b: arrays of time series
+        window: positive int, optional, default to None
+                size of the window so that only points within the range will be compared, hence limits the warping
+        returnLength: bool, optional, default to False
+                      whether to return the optimal path length. If set to True, the optimal path length is returned, and the resulting disctance will be divided by the length (sort of average distance between best matched points pairs)
+        '''
         self.compute_matrix(a, b, window)
         self.back_tracking()
         if returnLength:
             return self.distance/self.pathLength, self.pathLength
         else:
-            return self.distance #/self.pathLength
+            return self.distance
