21181984dc3e73194c7cbdc3df3d3e3b049c3e65
ChatCatKatzeNeko <wangsiyun6.15elsa@gmail.com>
2021-09-14 19:26:52 +0200

    major update to version 2

--- myDTW.py
+++ myDTW.py
@@ -1,12 +1,15 @@
 '''
 Author: Siyun WANG
+Version: 2
 '''
+
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 
 class myDTW():
     def __init__(self):
-        pass
+        self.switch = False
     
     def compute_matrix(self, a, b, window):
         
@@ -21,18 +24,64 @@ class myDTW():
                     val2 = min(self.distMat[i-1, j-1], self.distMat[i-1,j], self.distMat[i,j-1])
                     self.distMat[i,j] = val1 + val2
         else:
+            self.switch = len(a) > len(b)
             diff = abs(len(a)-len(b))
-            shorter = a if len(a) < len(b) else b
-            longer = a if len(a) >= len(b) else b
+            shorter = a if not self.switch else b
+            longer = a if self.switch else b
             self.a = shorter
             self.b = longer
             self.distMat = np.ones((len(shorter)+1,len(longer)+1))*np.inf
             self.distMat[0,0] = 0
             for i in range(1,len(shorter)+1):
-                for j in range(max(1, i-window+1),min(len(longer)+1,i+diff+window)): 
+                for j in range(max(1, i-window+1),min(len(longer)+1,i+diff+window)):
                     val1 = abs(shorter[i-1]-longer[j-1])
                     val2 = min(self.distMat[i-1, j-1], self.distMat[i-1,j], self.distMat[i,j-1])
                     self.distMat[i,j] = val1 + val2
+        self.distance = self.distMat[i, j]
+        
+    def compute_matrix_deprecated(self, a, b, window, correlationSize):
+        '''deprecated'''
+        if window is None:
+            self.a = a
+            self.b = b
+            self.distMat = np.ones((len(a)+1,len(b)+1))*np.inf
+            self.distMat[0,0] = 0
+            for i in range(1,len(a)+1):
+                for j in range(1,len(b)+1):
+                    val1 = abs(a[i-1]-b[j-1])
+                    val2 = min(self.distMat[i-1, j-1], self.distMat[i-1,j], self.distMat[i,j-1])
+                    # new: add correlation information into the distance matrix
+                    if correlationSize > 0:
+                        if (i>3) & (j>3): #(at least 3 points to get non-trivial correlation)
+                            validLen = min(correlationSize, i-1, j-1)
+                            val3 = np.corrcoef(a[i-validLen-1:i], b[j-validLen-1:j])[0,1]
+                        else:
+                            val3 = 0
+                    else:
+                        val3 = 0
+                    self.distMat[i,j] = val1 + val2 - val3
+        else:
+            self.switch = len(a) > len(b)
+            diff = abs(len(a)-len(b))
+            shorter = a if not self.switch else b
+            longer = a if self.switch else b
+            self.a = shorter
+            self.b = longer
+            self.distMat = np.ones((len(shorter)+1,len(longer)+1))*np.inf
+            self.distMat[0,0] = 0
+            for i in range(1,len(shorter)+1):
+                for j in range(max(1, i-window+1),min(len(longer)+1,i+diff+window)):
+                    val1 = abs(shorter[i-1]-longer[j-1])
+                    val2 = min(self.distMat[i-1, j-1], self.distMat[i-1,j], self.distMat[i,j-1])
+                    if correlationSize > 0:
+                        if (i>3) & (j>3): #(at least 3 points to get non-trivial correlation)
+                            validLen = min(correlationSize, i-1, j-1)
+                            val3 = np.corrcoef(shorter[i-validLen-1:i], longer[j-validLen-1:j])[0,1]
+                        else:
+                            val3 = 0
+                    else:
+                        val3 = 0
+                    self.distMat[i,j] = val1 + val2 - val3
         self.distance = self.distMat[i, j]
         
     def back_tracking(self):
@@ -41,16 +90,24 @@ class myDTW():
         i = self.distMat.shape[0] - 1
         j = self.distMat.shape[1] - 1
         
-        self.axis0 = [i]
-        self.axis1 = [j]
+        axis0 = [i]
+        axis1 = [j]
         while (i>1) | (j>1):
             i, j = self._helper_get_next_pivot(i, j)
-            self.axis0.append(i)
-            self.axis1.append(j)
+            axis0.append(i)
+            axis1.append(j)
             self.pathLength += 1
-             
-        self.axis0 = np.array(self.axis0)
-        self.axis1 = np.array(self.axis1)
+        
+        self.pathMat = np.zeros_like(self.distMat)
+        self.pathMat[axis0, axis1] = 1
+        self.pathMat = self.pathMat[1:,1:]
+        
+        if self.switch:
+            self.axis_a = np.array(axis1)
+            self.axis_b = np.array(axis0)
+        else:
+            self.axis_a = np.array(axis0)
+            self.axis_b = np.array(axis1)
         
     def _helper_get_next_pivot(self,i,j):
         tmpMat = self.distMat[i-1:i+1, j-1:j+1].copy()
@@ -60,11 +117,6 @@ class myDTW():
         return i-(1-minIndices[0]), j-(1-minIndices[1])
     
     def plot_optimal_path(self):
-        '''
-        plot the optimal back tracking path
-        '''
-        matcp = self.distMat.copy()
-        matcp[self.axis0, self.axis1] = 1e6 # initialising the distance matrix to a large number (1e6 in this case) in order to make the path stand out from the background when plotted
         fig = plt.figure(figsize=(12,12))
         G = plt.GridSpec(ncols=4,nrows=4,figure=fig)
         
@@ -75,25 +127,39 @@ class myDTW():
         axB.plot(self.b)
         axB.set(yticks=[])
         axPath = fig.add_subplot(G[1:,1:],sharey=axA,sharex=axB)
-        axPath.imshow(matcp[1:,1:],'gray')
+        axPath.imshow(self.pathMat,'gray')
         axPath.set(xticks=[], yticks=[])
         plt.show()
     
-    # "main" function of the class: compute the DTW distance
     def compute(self, a, b, window=None, returnLength=False):
-        '''
-        return the DTW distance between two input time series
-        
-        INPUTS:
-        a,b: arrays of time series
-        window: positive int, optional, default to None
-                size of the window so that only points within the range will be compared, hence limits the warping
-        returnLength: bool, optional, default to False
-                      whether to return the optimal path length. If set to True, the optimal path length is returned, and the resulting disctance will be divided by the length (sort of average distance between best matched points pairs)
-        '''
         self.compute_matrix(a, b, window)
         self.back_tracking()
         if returnLength:
             return self.distance/self.pathLength, self.pathLength
         else:
-            return self.distance
+            return self.distance #/self.pathLength
+
+    def match_period(self, start, end, a2b=True):
+        '''
+        matches a period from a curve to another
+
+        start, end: indices of the period to be matched
+        a2b: bool, default to True. If true, match a period from curve a's timeline to b's and vice versa if false.
+
+        returns the tuple of the matched period (start, end) of the other curve
+        '''
+        start = max(0, start)
+        if a2b:
+            if not self.switch:
+                end = min(end, len(self.a)-1)
+                return np.where(self.pathMat[start,:]==1)[0][0], np.where(self.pathMat[end,:]==1)[0][0]
+            else:
+                end = min(end, len(self.b)-1)
+                return np.where(self.pathMat[:,start]==1)[0][0], np.where(self.pathMat[:,end]==1)[0][0]
+        else:
+            if not self.switch:
+                end = min(end, len(self.b)-1)
+                return np.where(self.pathMat[:,start]==1)[0][0], np.where(self.pathMat[:,end]==1)[0][0]
+            else:
+                end = min(end, len(self.a)-1)
+                return np.where(self.pathMat[start,:]==1)[0][0], np.where(self.pathMat[end,:]==1)[0][0]
